e79d73d5fca642269f678477e10183ae220ffb7f
Katrina Ariola <kat3ariola@gmail.com>
2023-09-11 05:24:51 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React, { Component } from 'react'
+
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <button>Increment</button>
+    </>
+  )
+}
+
+export default App;
